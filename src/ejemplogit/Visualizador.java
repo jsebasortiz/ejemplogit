7cdc9320f5e2ba699daeb7d738386027221eddaa
Sebastian Ortiz <juan.ortiz05@uceva.edu.co>
2022-09-08 23:26:42 -0500

    Se agrego la clase visualizador

---
+++ src/ejemplogit/Visualizador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogit;
+
+/**
+ * Esta clase es un ejemplo
+ * @author juan sebastian ortiz <Juan.ortiz05@uceva.edu.co>
+ */
+public class Visualizador {
+    
+}
